739c2c80e5d7cc8833610dc85811518399f5f11b
Alex Tjokro <alex@tjok.ro>
2020-01-14 10:15:08 +0700

    Register footer menu

--- wp-content/themes/thinkertree/functions.php
+++ wp-content/themes/thinkertree/functions.php
@@ -58,6 +58,7 @@ if ( ! function_exists( 'thinkertree_setup' ) ) :
 		// This theme uses wp_nav_menu() in one location.
 		register_nav_menus( array(
 			'menu-1' => esc_html__( 'Primary', 'thinkertree' ),
+			'footer-menu' => esc_html__( 'Footer', 'thinkertree' ),
 		) );
 
 		/*
